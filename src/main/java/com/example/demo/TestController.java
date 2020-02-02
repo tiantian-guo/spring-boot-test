623bf77c5540ffa956d55e7179a47096176ca23d
tt <314152229@qq.com>
2020-02-02 13:11:54 +0800

    feature: add new

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +9,11 @@ public class TestController {
 
     @RequestMapping("/test1")
     public String test1(){
-
         return "hello word";
+    }
+
+    @GetMapping("/baobiao")
+    public String baobiao(){
+        return "我是报表分支";
     }
 }
